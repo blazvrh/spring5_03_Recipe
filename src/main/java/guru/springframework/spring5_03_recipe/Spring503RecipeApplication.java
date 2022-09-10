a46c96e81bc59b0f291ee78ccdd7d55d0e6bcc8d
KrHo <krhodev@gmail.com>
2022-09-10 21:46:15 +0200

    initial commit

---
+++ src/main/java/guru/springframework/spring5_03_recipe/Spring503RecipeApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5_03_recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring503RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring503RecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring5_03_recipe/Spring503RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5_03_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring503RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
