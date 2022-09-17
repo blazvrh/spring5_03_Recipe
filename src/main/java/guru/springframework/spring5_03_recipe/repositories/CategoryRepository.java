217a49fe8d0ffcc15038226570f08f0d3d1846df
KrHo <krhodev@gmail.com>
2022-09-17 23:40:22 +0200

    Query methods - JPA

--- src/main/java/guru/springframework/spring5_03_recipe/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/spring5_03_recipe/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package guru.springframework.spring5_03_recipe.repositories;
 import guru.springframework.spring5_03_recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findCategoryByDescription(String description);
 }
--- src/main/java/guru/springframework/spring5_03_recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/spring5_03_recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package guru.springframework.spring5_03_recipe.repositories;
 import guru.springframework.spring5_03_recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findUnitOfMeasureByDescription(String description);
 }
